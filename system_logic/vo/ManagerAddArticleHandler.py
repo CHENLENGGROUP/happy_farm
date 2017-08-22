4eef59ffd42aaae0ba8eef0c0245eda2492a3307
singlesjjj <30870777+singlesjjj@users.noreply.github.com>
2017-08-22 16:43:48 +0800

    addarticle

---
+++ system_logic/vo/ManagerAddArticleHandler.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import json
+import time
+import tornado
+import memcache
+import tornado.web
+import tornado.ioloop
+import tornado.gen
+from system_logic import setting
+from system_logic.vo.BaseHandler import BaseHandler
+from system_logic.bo.object.Manager import Manager
+from system_logic.vo.method.DecodeJson import _decode_dict
+
+class addArticletHandler(BaseHandler):
+
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
+    def get(self, *args, **kwargs):
+
+        if not self.get_login_status():
+            self.redirect('/managerlogin')
+            return
+
+        head_info = self.get_head_info('添加文章')
+
+        self.render('addarticle.html', head_info=head_info)
