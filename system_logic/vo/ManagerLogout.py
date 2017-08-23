e25313b69b8acfc9d3abfc0f4c2becefe24574de
singlesjjj <30870777+singlesjjj@users.noreply.github.com>
2017-08-23 16:10:13 +0800

    Logout

--- system_logic/vo/ManagerLogout.py
+++ system_logic/vo/ManagerLogout.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 
 import tornado
@@ -14,21 +13,4 @@ class ManagerLogOut(BaseHandler):
     def get(self, *args, **kwargs):
 
         self.log_out()
-=======
-# -*- coding: utf-8 -*-
-
-import tornado
-import tornado.web
-import tornado.ioloop
-import tornado.gen
-from system_logic.vo.BaseHandler import BaseHandler
-
-class ManagerLogOut(BaseHandler):
-
-    @tornado.web.asynchronous
-    @tornado.gen.coroutine
-    def get(self, *args, **kwargs):
-
-        self.log_out()
->>>>>>> dc5cf191e38ea9711dbc7643b5da028859ab8a09
         self.redirect('/managerlogin')
